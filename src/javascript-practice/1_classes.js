3aa594692579763a0628d4aa164126e1579a942c
justisGipson <justis710@gmail.com>
2018-12-22 16:55:15 -0500

    update

--- src/javascript-practice/1_classes.js
+++ src/javascript-practice/1_classes.js
@@ -1,1 +1,46 @@
-console.log('Classes file');
+// console.log('Classes'); 
+
+// ES6 JS Classes
+class User {
+    constructor(name){
+        this.name = name;
+        this.type = "Trial User"
+    }
+    // method 1
+    greet(){
+        console.log('Welcome Back, ' + this.name);
+    }
+    // method 2
+    status(){
+        console.log('Current status: ' + this.type);
+    }
+}
+
+//Instance of the class/new object
+var anonDude = new User("Anonymous dude");
+
+//we can now use the instance and the . operator 
+// to access the 2 methods
+anonDude.greet();
+anonDude.status();
+
+//Another instance of the class
+var anonLady = new User("Anonymous lady");
+anonLady.greet();
+anonLady.status();
+
+ //Another instance of the class
+var jeff = new User("Jeff");
+jeff.greet();
+jeff.status();
+
+var anonPerson = new User ("Anonymous person");
+
+anonPerson.greet();
+anonPerson.status();
+
+var anonDog = new User ("Anonymous Dog");
+
+anonDog.greet();
+anonDog.status();
+
