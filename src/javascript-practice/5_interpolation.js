45fb2c1786f0e223608f014be1399869945c4315
justisGipson <justis710@gmail.com>
2018-12-22 22:32:34 -0500

    changes

---
+++ src/javascript-practice/5_interpolation.js
@@ -0,0 +1,13 @@
+const username = 'Justis';
+console.log(`${username} is a true JS wizard!`);
+
+console.log(`Hey students,
+what bugs can
+${username}
+fix today?`)
+
+const person = {
+    username: 'Justis',
+    profession: 'Secret Agent'
+}
+console.log(`${person.username}, ${person.profession} extraordinaire.`)
