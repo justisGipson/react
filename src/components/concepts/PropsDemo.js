10b8ea4147561a23c00215963cbf2638f663453a
justisGipson <justis710@gmail.com>
2018-12-25 13:29:57 -0500

    update props

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 export default class PropsDemo extends React.Component {
+    constructor(){
+        super();
+        this.state ={
+            color: 'white',
+            backgroundColor: 'blue',
+            borderRadius: '5px',
+            borderStyle: 'dashed',
+            display: 'inline-block',
+            width: '300px',
+            textAlign: 'center'
+        }
+    }
+
+    colorToggle = () => {
+        this.setState({
+            color: this.state.color === 'white' ? 'red' : 'white'
+        })
+    }
+
     render() {
         return (
-            <div>
-                <FunctionalComp string="will this display?"/>
+            <div className='main'>
+                <div className='mainDiv'>
+                    <div style={this.state}>
+                        <FunctionalComp string="will this display?"/>
+                        <FunctionalComp string='Star Wars over Star Trek'/>
+                        <FunctionalComp string='Programming blah blah blah'/>
+                        <FunctionalComp string='Dune is the greatest Sci-fi movie'/>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -14,6 +40,7 @@ const FunctionalComp = (props) => {
     return(
         <div>
             <p>{props.string}</p>
+            <button onClick={props.function}>Press Me!</button>
         </div>
     )
 }
