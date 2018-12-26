ed11d5259017f22d90127feed48e66a08d7b084a
justisGipson <justis710@gmail.com>
2018-12-25 20:01:07 -0500

    style buttons

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -20,15 +20,33 @@ export default class PropsDemo extends React.Component {
         })
     }
 
+    backgroundColorToggle = () => {
+        this.setState({
+            backgroundColor: this.state.backgroundColor === 'blue' ? 'green' : 'blue'
+        })
+    }
+
+    borderRadiusToggle = () => {
+        this.setState({
+            borderRadius: this.state.borderRadius === '5px' ? '25px' : '5px'
+        })
+    }
+
+    borderStyleToggle = () => {
+        this.setState({
+            borderStyle: this.state.borderStyle === 'dashed' ? 'solid' : 'dashed'
+        })
+    }
+
     render() {
         return (
             <div className='main'>
                 <div className='mainDiv'>
                     <div style={this.state}>
-                        <FunctionalComp string="will this display?"/>
-                        <FunctionalComp string='Star Wars over Star Trek'/>
-                        <FunctionalComp string='Programming blah blah blah'/>
-                        <FunctionalComp string='Dune is the greatest Sci-fi movie'/>
+                        <FunctionalComp string='Color Toggle' function={this.colorToggle}/>
+                        <FunctionalComp string='Background Color Toggle' function={this.backgroundColorToggle}/>
+                        <FunctionalComp string='Border Radius Toggle' function={this.borderRadiusToggle}/>
+                        <FunctionalComp string='Border Style Toggle' function={this.borderStyleToggle}/>
                     </div>
                 </div>
             </div>
